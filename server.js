f0758e7c16eed87f4a42cb5fc14be7044f04b8c3
Moukis-mab <103313495+Moukis-mab@users.noreply.github.com>
2022-06-26 07:42:39 +0100

    commit-status

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = new express()
 const port = process.env.port || 80
 
 app.get('/', (req, res)=>{
-    res.send("Welcome to my web service")
+    res.status(200).send("Welcome to my web service")
 })
 
 app.listen(port, ()=>{console.log("server running on port : " + port)})
